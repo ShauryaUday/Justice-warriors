664945ba0cc246cddea26187545dd9b1e6e30ae8
ShauryaUday <68910776+ShauryaUday@users.noreply.github.com>
2020-10-26 20:59:03 +0530

    Add files via upload

---
+++ fifthpage.js
@@ -0,0 +1,13 @@
+class Fifthpage{
+    constructor(){
+        this.title=createElement('h1');
+        this.title.html("Report with just text");
+        this.input=createInput("Write your Report here:)");
+        this.submit=createButton('Submit');
+    }
+    display(){
+        this.title.position(200,150);
+        this.input.position(200,200);
+        this.submit.position(200,300);
+    }
+}
